b4ad6829d84688018086e5efd883bbd658b6aeee
Tyler Whitsett <whitsett.tyler@gmail.com>
2015-02-27 11:32:25 -0800

    add base file structure.

---
+++ app.js
@@ -0,0 +1,15 @@
+var newsApp = angular.module('newsApp', ['ui.router']);
+
+newsApp.config(function($stateProvider) {
+  $stateProvider.state('home', {
+    url: "",
+    templateUrl: "partials/home.html"
+  });
+
+  $stateProvider.state('beats', {
+    url: "/beats",
+    templateUrl: "partials/beats.html",
+    controller: 'BeatsCtrl'
+  });
+
+});
